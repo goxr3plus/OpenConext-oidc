ac138d012915cd1c487bcb15f988675e7dc8c3a0
GOXR3PLUS <crAlexcomp@gmail.com>
2017-08-29 11:46:04 +0300

    Trying to add Manage Services

--- oidc-server/src/main/java/oidc/config/ClientsAndResourcesInitializer.java
+++ oidc-server/src/main/java/oidc/config/ClientsAndResourcesInitializer.java
@@ -23,108 +23,108 @@ import java.util.*;
 @PropertySource("classpath:application.oidc.properties")
 public class ClientsAndResourcesInitializer implements InitializingBean {
 
-  private final Logger LOG = LoggerFactory.getLogger(ClientsAndResourcesInitializer.class);
+    private final Logger LOG = LoggerFactory.getLogger(ClientsAndResourcesInitializer.class);
 
-  private final Resource clientConfLocation;
+    private final Resource clientConfLocation;
 
-  private final TransactionTemplate transactionTemplate;
+    private final TransactionTemplate transactionTemplate;
 
-  private final OAuth2ClientRepository clientRepository;
+    private final OAuth2ClientRepository clientRepository;
 
-  @Autowired
-  public ClientsAndResourcesInitializer(@Value("${client.conf.location}") Resource clientConfLocation, PlatformTransactionManager transactionManager, OAuth2ClientRepository clientRepository) {
-    this.clientConfLocation = clientConfLocation;
-    this.transactionTemplate = new TransactionTemplate(transactionManager);
-    this.clientRepository = clientRepository;
-  }
-
-  @Override
-  public void afterPropertiesSet() throws Exception {
-    try {
-      applicationEvent();
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+    @Autowired
+    public ClientsAndResourcesInitializer(@Value("${client.conf.location}") Resource clientConfLocation, PlatformTransactionManager transactionManager, OAuth2ClientRepository clientRepository) {
+        this.clientConfLocation = clientConfLocation;
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
+        this.clientRepository = clientRepository;
     }
-  }
 
-  protected List<ClientDetailsEntity> applicationEvent() throws IOException {
-    Yaml yaml = new Yaml();
-    final List<ClientDetailsEntity> clientDetails = new ArrayList<>();
-    Map<String, List<Map<String, Object>>> config = (Map<String, List<Map<String, Object>>>) yaml.load(clientConfLocation.getInputStream());
-    List<Map<String, Object>> clients = config.get("clients");
-    for (int i = 0; i < clients.size(); i++) {
-      clientDetails.add(client(clients.get(i)));
-    }
-    List<Map<String, Object>> resourceServers = config.get("resourceServers");
-    for (int i = 0; i < resourceServers.size(); i++) {
-      ClientDetailsEntity entity = resourceServer(resourceServers.get(i));
-      ClientDetailsEntity clientDetailsEntity = configuredClient(entity, clientDetails);
-      if (clientDetailsEntity != null) {
-        clientDetailsEntity.setAllowIntrospection(true);
-      } else {
-        clientDetails.add(entity);
-      }
-    }
-    return transactionTemplate.execute(new TransactionCallback<List<ClientDetailsEntity>>() {
-      @Override
-      public List<ClientDetailsEntity> doInTransaction(TransactionStatus transactionStatus) {
-        for (int i = 0; i < clientDetails.size(); i++) {
-          ClientDetailsEntity entity = clientDetails.get(i);
-          ClientDetailsEntity existing = clientRepository.getClientByClientId(entity.getClientId());
-          if (existing == null) {
-            LOG.debug("Inserting new default client {}", entity.getClientId());
-            clientRepository.saveClient(entity);
-          } else {
-            LOG.debug("Updating new default client {}", entity.getClientId());
-            clientRepository.updateClient(existing.getId(), entity);
-          }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        try {
+            applicationEvent();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return clientDetails;
-      }
-    });
-  }
+    }
 
-  private ClientDetailsEntity configuredClient(ClientDetailsEntity resourceServer, List<ClientDetailsEntity> clientDetails) {
-    for (int i = 0; i < clientDetails.size(); i++) {
-      ClientDetailsEntity clientDetailsEntity = clientDetails.get(i);
-      if (clientDetailsEntity.getClientId().equals(resourceServer.getClientId())) {
+    protected List<ClientDetailsEntity> applicationEvent() throws IOException {
+        Yaml yaml = new Yaml();
+        final List<ClientDetailsEntity> clientDetails = new ArrayList<>();
+        Map<String, List<Map<String, Object>>> config = (Map<String, List<Map<String, Object>>>) yaml.load(clientConfLocation.getInputStream());
+        List<Map<String, Object>> clients = config.get("clients");
+        for (int i = 0; i < clients.size(); i++) {
+            clientDetails.add(client(clients.get(i)));
+        }
+        List<Map<String, Object>> resourceServers = config.get("resourceServers");
+        for (int i = 0; i < resourceServers.size(); i++) {
+            ClientDetailsEntity entity = resourceServer(resourceServers.get(i));
+            ClientDetailsEntity clientDetailsEntity = configuredClient(entity, clientDetails);
+            if (clientDetailsEntity != null) {
+                clientDetailsEntity.setAllowIntrospection(true);
+            } else {
+                clientDetails.add(entity);
+            }
+        }
+        return transactionTemplate.execute(new TransactionCallback<List<ClientDetailsEntity>>() {
+            @Override
+            public List<ClientDetailsEntity> doInTransaction(TransactionStatus transactionStatus) {
+                for (int i = 0; i < clientDetails.size(); i++) {
+                    ClientDetailsEntity entity = clientDetails.get(i);
+                    ClientDetailsEntity existing = clientRepository.getClientByClientId(entity.getClientId());
+                    if (existing == null) {
+                        LOG.debug("Inserting new default client {}", entity.getClientId());
+                        clientRepository.saveClient(entity);
+                    } else {
+                        LOG.debug("Updating new default client {}", entity.getClientId());
+                        clientRepository.updateClient(existing.getId(), entity);
+                    }
+                }
+                return clientDetails;
+            }
+        });
+    }
+
+    private ClientDetailsEntity configuredClient(ClientDetailsEntity resourceServer, List<ClientDetailsEntity> clientDetails) {
+        for (int i = 0; i < clientDetails.size(); i++) {
+            ClientDetailsEntity clientDetailsEntity = clientDetails.get(i);
+            if (clientDetailsEntity.getClientId().equals(resourceServer.getClientId())) {
+                return clientDetailsEntity;
+            }
+        }
+        return null;
+    }
+
+    private ClientDetailsEntity resourceServer(Map<String, Object> data) {
+        ClientDetailsEntity resourceServer = doGetClientDetails(data);
+        resourceServer.setAllowIntrospection(true);
+        return resourceServer;
+    }
+
+    private ClientDetailsEntity client(Map<String, Object> data) {
+        ClientDetailsEntity client = doGetClientDetails(data);
+        List<String> redirectUris = (List<String>) data.get("redirectUris");
+        if (redirectUris != null) {
+            client.setRedirectUris(new HashSet<>(redirectUris));
+        }
+        List<String> grantTypes = (List<String>) data.get("grantTypes");
+        if (grantTypes != null) {
+            client.setGrantTypes(new HashSet<>(grantTypes));
+        }
+        return client;
+    }
+
+    private ClientDetailsEntity doGetClientDetails(Map<String, Object> data) {
+        ClientDetailsEntity clientDetailsEntity = new ClientDetailsEntity();
+        clientDetailsEntity.setClientId((String) data.get("client_id"));
+        clientDetailsEntity.setClientSecret((String) data.get("secret"));
+        clientDetailsEntity.setCreatedAt(new Date());
+        clientDetailsEntity.setAccessTokenValiditySeconds(86400);
+        List<String> scopes = (List<String>) data.get("scopes");
+        if (scopes != null) {
+            clientDetailsEntity.setScope(new HashSet<>(scopes));
+        }
+
         return clientDetailsEntity;
-      }
     }
-    return null;
-  }
-
-  private ClientDetailsEntity resourceServer(Map<String, Object> data) {
-    ClientDetailsEntity resourceServer = doGetClientDetails(data);
-    resourceServer.setAllowIntrospection(true);
-    return resourceServer;
-  }
-
-  private ClientDetailsEntity client(Map<String, Object> data) {
-    ClientDetailsEntity client = doGetClientDetails(data);
-    List<String> redirectUris = (List<String>) data.get("redirectUris");
-    if (redirectUris != null) {
-      client.setRedirectUris(new HashSet<>(redirectUris));
-    }
-    List<String> grantTypes = (List<String>) data.get("grantTypes");
-    if (grantTypes != null) {
-      client.setGrantTypes(new HashSet<>(grantTypes));
-    }
-    return client;
-  }
-
-  private ClientDetailsEntity doGetClientDetails(Map<String, Object> data) {
-    ClientDetailsEntity clientDetailsEntity = new ClientDetailsEntity();
-    clientDetailsEntity.setClientId((String) data.get("client_id"));
-    clientDetailsEntity.setClientSecret((String) data.get("secret"));
-    clientDetailsEntity.setCreatedAt(new Date());
-    clientDetailsEntity.setAccessTokenValiditySeconds(86400);
-    List<String> scopes = (List<String>) data.get("scopes");
-    if (scopes != null) {
-      clientDetailsEntity.setScope(new HashSet<>(scopes));
-    }
-
-    return clientDetailsEntity;
-  }
 
 }
